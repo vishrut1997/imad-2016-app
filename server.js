bc180067464f4cb77600a0ded3726d82072895ab
vishrut1997 <vishrut1997@gmail.com>
2016-11-05 13:15:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-var app = express
+var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
